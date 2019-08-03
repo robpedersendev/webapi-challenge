652fcd53518d32faa912f596e8074f61d87c1f01
RobertPedersenDev <40696979+robpedersendev@users.noreply.github.com>
2019-08-03 11:55:18 -0700

    Created routers/ directory with subfolders and files
    Within the projects/index.js I have added the initial structure for the file. Next I will add in the API CRUD functionality

---
+++ routers/projects/index.js
@@ -0,0 +1,13 @@
+    //Assigns router to the value of the expressJS router method 
+    const router = require('express').Router()
+        //This takes all routes from the actions/index file and assigns them to actionsRouter
+    const actionsRouter = require('../actions')
+        //This assigns all database functions to the db variable
+    const db = require('../../data/helpers/projectModel')
+
+    /*
+    This block needs to be updated
+        //This line is importing the validateUser and validateUserId from the middleware file 
+    const { validateUserId, validateUser } = require('../../middleware')
+
+    */
