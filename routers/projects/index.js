f9d5bf8a9f4506217db12ff81deab9470d34f09c
RobertPedersenDev <40696979+robpedersendev@users.noreply.github.com>
2019-08-03 12:20:52 -0700

    Added comments to the projects/index file

--- routers/projects/index.js
+++ routers/projects/index.js
@@ -1,4 +1,8 @@
+//This line allows us to use the express framework for node.js
 const express = require("express");
+//This line will allow us to use the router method later on
 const router = express.Router();
+//This imports the actions db
 const actionsdb = require("../data/helpers/actionModel");
+//This imports the projects db
 const projectsdb = require("../data/helpers/projectModel");
